60901f6758d37482bb085b58db36400d58ff06ae
asdi80 <a8sadi@gmail.com>
2022-01-13 20:15:10 -0700

    update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,31 @@
 INSERT INTO department (name)
 VALUES 
+('HR'),
 ('IT'),
 ('Finance & Accounting'),
 ('Sales & Marketing'),
 ('Operations');
 
+
 INSERT INTO role (title, salary, department_id)
 VALUES
 ('Full Stack Developer', 80000, 1),
-('Software Engineer', 120000, 1),
 ('Accountant', 10000, 2), 
 ('Finanical Analyst', 150000, 2),
-('Marketing Coordindator', 70000, 3), 
 ('Sales Lead', 90000, 3),
 ('Project Manager', 100000, 4),
 ('Operations Manager', 90000, 4);
+('Software Engineer', 120000, 1),
+('Marketing Coordindator', 70000, 3), 
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('Mark', 'Miller', 2, null),
-('Devin', 'Anderson', 1, 1),
+('John', 'Jo', 2, null),
+('Rafi', 'Roufa', 1, 1),
 ('Mary', 'Brown', 4, null),
-('Ashley', 'Jones', 3, 3),
-('Tyler', 'Moore', 6, null),
-('Ana', 'Sanchez', 5, 5),
+('Ash', 'John', 3, 3),
+('Eiko', 'Moo', 6, null),
+('Abdurraouf', 'Sadi', 5, 5),
 ('Lewis', 'Allen', 7, null),
-('Katherine', 'Green', 8, 7);
+('Tom', 'Tom', 8, 7);
